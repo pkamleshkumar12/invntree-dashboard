aa9219a7c30c6c40a40c5dc9c24952f4cabe8110
kamlesh <pkamleshkumar12@gmail.com>
2024-09-29 20:47:19 +1300

    setting up navbar and sidebar

--- inventory-dashboard/src/app/page.tsx
+++ inventory-dashboard/src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
-
+import Dashboard from "@/app/dashboard/page"
 export default function Home() {
-  return (
-     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-       hello
-     </main>
-  );
+  return <Dashboard />;
 }
