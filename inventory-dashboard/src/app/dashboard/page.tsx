1e21d7ba61bf729d388a84e0265735cd09d4ae52
kamlesh <pkamleshkumar12@gmail.com>
2024-10-07 11:49:04 +1300

    dashboard components: CardExpenseSummary, CardPopularProduct, CardPurchaseSummary, CardSalesSummary

--- inventory-dashboard/src/app/dashboard/page.tsx
+++ inventory-dashboard/src/app/dashboard/page.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+"use client"
+
+import CardPopularProduct from "@/app/dashboard/CardPopularProduct";
+import CardPurchaseSummary from "@/app/dashboard/CardPurchaseSummary";
+import CardSalesSummary from "@/app/dashboard/CardSalesSummary";
+import CardExpenseSummary from "@/app/dashboard/cardExpenseSummary";
 
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard
-        </div>
+
+            <div
+                    className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows"
+            >
+                <CardPopularProduct />
+                <CardSalesSummary />
+                <CardPurchaseSummary />
+
+                <CardExpenseSummary />
+                <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
+                <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
+                <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
+            </div>
     );
 };
 
--- inventory-dashboard/src/state/api.ts
+++ inventory-dashboard/src/state/api.ts
@@ -56,13 +56,28 @@ export interface User {
     email: string;
 }
 
+export interface DashboardMetrics {
+    popularProducts: Product[];
+    salesSummary: SalesSummary[];
+    purchaseSummary: PurchaseSummary[];
+    expenseSummary: ExpenseSummary[];
+    expenseByCategorySummary: ExpenseByCategorySummary[];
+}
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
     reducerPath: "api",
-    tagTypes: ["DashboardMetrics", "Products", "Users", "Expenses"],
-    endpoints: (build) => ({
+    tagTypes: ["DashboardMetrics"],
+    endpoints: (builder) => ({
+        getDashboardMetrics: builder.query<DashboardMetrics, void>({
+            query: () => "/dashboard",
+            providesTags: ["DashboardMetrics"],
+        }),
     }),
+
 });
 
+
 export const {
+    useGetDashboardMetricsQuery,
 } = api;
