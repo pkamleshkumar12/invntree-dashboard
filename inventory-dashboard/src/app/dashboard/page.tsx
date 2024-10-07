75397ef57fa847c2729e0f2e414d8aef8c74075b
kamlesh <pkamleshkumar12@gmail.com>
2024-10-07 14:10:59 +1300

    dashboard component: statCard demo

--- inventory-dashboard/src/app/dashboard/page.tsx
+++ inventory-dashboard/src/app/dashboard/page.tsx
@@ -4,21 +4,76 @@ import CardPopularProduct from "@/app/dashboard/CardPopularProduct";
 import CardPurchaseSummary from "@/app/dashboard/CardPurchaseSummary";
 import CardSalesSummary from "@/app/dashboard/CardSalesSummary";
 import CardExpenseSummary from "@/app/dashboard/cardExpenseSummary";
-
+import {CheckCircle, Package, Tag, TrendingDown, TrendingUp} from "lucide-react";
+import StatCard from "@/app/dashboard/StatCard";
 const Dashboard = () => {
     return (
 
             <div
                     className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows"
             >
-                <CardPopularProduct />
-                <CardSalesSummary />
-                <CardPurchaseSummary />
+                <CardPopularProduct/>
+                <CardSalesSummary/>
+                <CardPurchaseSummary/>
 
-                <CardExpenseSummary />
-                <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
-                <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
-                <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
+                <CardExpenseSummary/>
+                <StatCard
+                        title="Customer & Expenses [Demo] "
+                        primaryIcon={<Package className="text-blue-600 w-6 h-6"/>}
+                        dateRange="22 - 29 October 2024"
+                        details={[
+                            {
+                                title: "Customer Growth",
+                                amount: "175.00",
+                                changePercentage: 131,
+                                IconComponent: TrendingUp,
+                            },
+                            {
+                                title: "Expenses",
+                                amount: "10.00",
+                                changePercentage: -56,
+                                IconComponent: TrendingDown,
+                            },
+                        ]}
+                />
+                <StatCard
+                        title="Dues & Pending Orders [Demo] "
+                        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6"/>}
+                        dateRange="22 - 29 October 2024"
+                        details={[
+                            {
+                                title: "Dues",
+                                amount: "250.00",
+                                changePercentage: 131,
+                                IconComponent: TrendingUp,
+                            },
+                            {
+                                title: "Pending Orders",
+                                amount: "147",
+                                changePercentage: -56,
+                                IconComponent: TrendingDown,
+                            },
+                        ]}
+                />
+                <StatCard
+                        title="Sales & Discount [Demo]"
+                        primaryIcon={<Tag className="text-blue-600 w-6 h-6"/>}
+                        dateRange="22 - 29 October 2024"
+                        details={[
+                            {
+                                title: "Sales",
+                                amount: "1000.00",
+                                changePercentage: 20,
+                                IconComponent: TrendingUp,
+                            },
+                            {
+                                title: "Discount",
+                                amount: "200.00",
+                                changePercentage: -10,
+                                IconComponent: TrendingDown,
+                            },
+                        ]}
+                />
             </div>
     );
 };
