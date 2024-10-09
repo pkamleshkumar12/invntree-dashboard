830329c230b10dd46d49a793325e062caa0623ce
kamlesh <pkamleshkumar12@gmail.com>
2024-10-10 11:54:56 +1300

    updated minor changes readme.md

---
+++ inventory-server/prisma/migrations/20241009225429_init/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "ExpenseByCategory" DROP CONSTRAINT "ExpenseByCategory_expenseSummaryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Purchases" DROP CONSTRAINT "Purchases_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Purchases" ADD CONSTRAINT "Purchases_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("productId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ExpenseByCategory" ADD CONSTRAINT "ExpenseByCategory_expenseSummaryId_fkey" FOREIGN KEY ("expenseSummaryId") REFERENCES "ExpenseSummary"("expenseSummaryId") ON DELETE CASCADE ON UPDATE CASCADE;
